87463bad04b9003add31c7e7bf878e04bd2d4eaa
Venderizza <karinad2016god@gmail.com>
2024-11-21 12:01:41 +0400

    второе начало

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['pages/*.html'],
   theme: {
-    extend: {},
+    screens: {},
+
+    fontFamily: {
+      'sans': ['Gilroy-ExtraBold'],
+    },
+    
+    extend: {
+
+      backgroundImage: {
+        'main': "url('src/images/main.jfif')"
+      },
+      colors: {
+        blurButton: '#1C1C1C8F',
+        button: '#1C1C1C',
+        darkText: '#1E1E1E',
+        blurPink: '#DC02C373',
+        caseBlue: '#2039D2',
+      }
+
+      
+    },
   },
   plugins: [],
 }
